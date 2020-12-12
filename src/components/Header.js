a7bb27f836c27aba76726de607eaec5174a43f71
cascutter <cascutter13@gmail.com>
2020-12-12 17:22:14 -0600

    Adjusted styling, fixed routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,23 +8,23 @@ function Header() {
 
     return (
         <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+        <nav style={{ fontSize: 30, alignContent: "center" }} className="navbar navbar-expand-lg navbar-light bg-light">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
                     <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
                     About
                     </Link>
                     </li>
-                    <li class="nav-item">
-                    <Link to="/about" className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
+                    <li className="nav-item">
+                    <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
                     Portfolio
                     </Link>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                     <Link to="/contact" className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
                     Contact
                     </Link>
