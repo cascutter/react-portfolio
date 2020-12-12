4d0b49f8670fa0999eeb6eef9d85fca5ad17fe62
cascutter <cascutter13@gmail.com>
2020-12-11 18:28:33 -0600

    Portfolio page boilerplate

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "../styles/style.css";
 
 const Portfolio = () => (
-  
+    <div class="container" id="portfolioCards">
+    <div class="col-sm-12">
+        <section>
+            <h3>Collaborative Projects</h3>
+        </section>
+    </div>
+    </div>
 );
 
 export default Portfolio;
